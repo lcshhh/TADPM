16fb0e38958b02d8f20f5e83d2287ea5d25f1f96
leics19 <leics19@mails.tsinghua.edu.cn>
2024-10-17 10:37:52 +0800

    s

--- scripts/test_global.sh
+++ scripts/test_global.sh
@@ -1,6 +1,6 @@
 ENT="python second_stage.py "
 cfg="config/ae.yaml"
-ckpts="/data3/leics/dataset/checkpoints/diffusion/DiffusionVAE_newloss/ckpt-last.pth"
+ckpts="/data3/leics/dataset/checkpoints/voxel/residual_drop0.3/ckpt-best.pth"
 saveroot="/data3/leics/dataset/checkpoints/global"
 exp_name="vqvae"
 launcher="none"
